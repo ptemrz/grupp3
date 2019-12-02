93dca55376e94923d74a81a4ae480f0ca5a7859e
Axel Eriksson <a.eriksson0303@gmail.com>
2019-12-02 14:42:57 +0100

    added toString override for formating

--- src/vp/Card.java
+++ src/vp/Card.java
@@ -16,4 +16,42 @@ public class Card {
 	public Suit getSuit() {
 		return suit;
 	}
+	
+	@Override
+	public String toString() {
+		String prettyValue;
+		switch(value) {
+		case 1:
+			prettyValue = "A";
+			break;
+		case 10:
+			prettyValue = "T";
+			break;
+		case 11:
+			prettyValue = "J";
+			break;
+		case 12:
+			prettyValue = "Q";
+			break;
+		case 13:
+			prettyValue = "K";
+			break;
+		default:
+			prettyValue = "" + value;
+		}
+		
+		switch (suit) {
+		case HEARTS:
+			return prettyValue + "♥";
+		case SPADES:
+			return prettyValue + "♠";
+		case DIAMONDS:
+			return prettyValue + "♦";
+		case CLUBS:
+			return prettyValue + "♣";
+		default:
+			throw new RuntimeException("Vad är " + suit +"?");
+		}
+		
+	}
 }
