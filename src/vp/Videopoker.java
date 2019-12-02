e44bba6a3ca50509b4c3146d0918ca3472927632
Axel Eriksson <a.eriksson0303@gmail.com>
2019-12-02 16:06:15 +0100

    added initiation of Deck class in Main.java and created resetGame method

--- src/vp/Videopoker.java
+++ src/vp/Videopoker.java
@@ -1,5 +1,20 @@
 package vp;
 
-public class Videopoker {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Videopoker {
+	
+	Deck deck = new Deck();
+	List<Card> hand = new ArrayList<>();
+	
+	public void resetGame() {
+		
+		deck.shuffle();
+		
+		while (hand.size() < 5) {
+			hand.add(deck.draw());
+		}
+		
+	}
 }
