67ca99e28cfedb7a77de8235abd29d25a74b7a6c
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2019-12-05 15:54:22 +0100

    La till metod som letar efter par i pokerhand

--- src/vp/Videopoker.java
+++ src/vp/Videopoker.java
@@ -4,17 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Videopoker {
-	
+
 	Deck deck = new Deck();
 	List<Card> hand = new ArrayList<>();
-	
+
 	public void resetGame() {
-		
+
 		deck.shuffle();
-		
+
 		while (hand.size() < 5) {
 			hand.add(deck.draw());
 		}
-		
+
+	}
+
+	// Metoden letar efter par, förutsätter att handen är sorterad
+	private boolean isPair(List<Card> hand) {
+		for (int i = 0; i < (hand.size() - 1); i++) {
+			if (hand.get(i) == hand.get(i + 1)) {
+				return true;
+			}
+
+		}
+		return false;
 	}
 }
