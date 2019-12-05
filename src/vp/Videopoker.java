bd3c85c13e5c1ca24ee8df74aafced316993a61b
Axel Eriksson <a.eriksson0303@gmail.com>
2019-12-05 07:34:40 +0100

    added method isThreeOfAKind

--- src/vp/Videopoker.java
+++ src/vp/Videopoker.java
@@ -12,7 +12,7 @@ public class Videopoker {
 	public void resetGame() {
 
 		deck.shuffle();
-
+		hand.clear();
 		while (hand.size() < 5) {
 			hand.add(deck.draw());
 		}
@@ -21,7 +21,7 @@ public class Videopoker {
 	/**
 	 * Checks if the given hand is a full house
 	 * 
-	 * @return true iff the hand is a full house
+	 * @return true if the hand is a full house
 	 * @param hand must be a sorted list of cards
 	 */
 	private boolean isFullHouse(List<Card> hand) {
@@ -39,5 +39,29 @@ public class Videopoker {
 		}
 
 		return true;
+	}
+	
+	/**
+	 * Checks if the given hand is a three of a kind
+	 * 
+	 * @return true if the hand is a three of a kind
+	 * @param hand must be a sorted list of cards (assuming sorting by value)
+	 */
+	private boolean isThreeOfAKind(List<Card>hand) {
+		
+		if(hand.get(0).getValue() == hand.get(1).getValue() || hand.get(0).getValue() == hand.get(2).getValue()) {
+			return true;
+		}
+		
+		if (hand.get(1).getValue() == hand.get(2).getValue() || hand.get(1).getValue() == hand.get(3).getValue()) {
+			return true;
+		}
+		
+		if (hand.get(2).getValue() == hand.get(3).getValue() || hand.get(2).getValue() == hand.get(4).getValue()) {
+			return true;
+		}
+		
+		return false;
+		
 	}
 }
