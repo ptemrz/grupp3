a5c4cee8f75c89528bd78d4df152ab9e643b35cf
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2019-12-06 11:35:21 +0100

    Created successfull test for Straight. Test for StraightFlush tstill unsuccessfull.

--- src/vp/VideopokerTest.java
+++ src/vp/VideopokerTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class VideopokerTest {
 
 	private Deck deck;
@@ -18,10 +17,12 @@ class VideopokerTest {
 	void newDeck() {
 		deck = new Deck();
 	}
+
 	@BeforeEach
 	void newVideopoker() {
 		videopoker = new Videopoker();
 	}
+
 	@Test
 	void testIsPair() {
 		boolean asume = true;
@@ -35,20 +36,19 @@ class VideopokerTest {
 		assertEquals(asume, val);
 	}
 
-
 	@Test
 	void testIsFlush() {
-	
+
 		boolean ärDetFärg = true;
 		videopoker.hand.add(new Card(1, Suit.HEARTS));
 		videopoker.hand.add(new Card(5, Suit.HEARTS));
 		videopoker.hand.add(new Card(3, Suit.HEARTS));
 		videopoker.hand.add(new Card(6, Suit.HEARTS));
 		videopoker.hand.add(new Card(2, Suit.HEARTS));
-		
+
 		assertEquals(ärDetFärg, videopoker.isFlush(videopoker.hand));
 	}
-		
+
 	@Test
 	void testIsJQKAPair() {
 		boolean asume = true;
@@ -61,6 +61,7 @@ class VideopokerTest {
 		val = videopoker.isJQKAPair(videopoker.hand);
 		assertEquals(asume, val);
 	}
+
 	@Test
 	void testDoublePair() {
 		boolean asume = true;
@@ -73,18 +74,19 @@ class VideopokerTest {
 		val = videopoker.isTwoPair(videopoker.hand);
 		assertEquals(asume, val);
 	}
-//	@Test
-//	void testStraight() {
-//		boolean asume = true;
-//		boolean val;
-//		videopoker.hand.add(new Card(2, Suit.HEARTS));
-//		videopoker.hand.add(new Card(3, Suit.HEARTS));
-//		videopoker.hand.add(new Card(4, Suit.SPADES));
-//		videopoker.hand.add(new Card(5, Suit.HEARTS));
-//		videopoker.hand.add(new Card(6, Suit.HEARTS));
-//		val = videopoker.isStraight(videopoker.hand);
-//		assertEquals(asume, val);
-//	}
+
+	@Test
+	void testStraight() {
+		boolean asume = true;
+		boolean val;
+		videopoker.hand.add(new Card(2, Suit.HEARTS));
+		videopoker.hand.add(new Card(3, Suit.HEARTS));
+		videopoker.hand.add(new Card(4, Suit.SPADES));
+		videopoker.hand.add(new Card(5, Suit.HEARTS));
+		videopoker.hand.add(new Card(6, Suit.HEARTS));
+		val = videopoker.isStraight(videopoker.hand);
+		assertEquals(asume, val);
+	}
 	@Test
 	void testRoyalStraightFlush() {
 		boolean asume = true;
@@ -96,30 +98,31 @@ class VideopokerTest {
 		videopoker.hand.add(new Card(13, Suit.HEARTS));
 		val = videopoker.isRoyalStraightFlush(videopoker.hand);
 		assertEquals(asume, val);
-
 	}
-//	@Test
-//	void testStraightFlush() {
-//		boolean asume = true;
-//		boolean val;
-//		videopoker.hand.add(new Card(4, Suit.HEARTS));
-//		videopoker.hand.add(new Card(5, Suit.HEARTS));
-//		videopoker.hand.add(new Card(6, Suit.HEARTS));
-//		videopoker.hand.add(new Card(7, Suit.HEARTS));
-//		videopoker.hand.add(new Card(8, Suit.HEARTS));
-//		val = videopoker.isStraightFlush(videopoker.hand);
-//		assertEquals(asume, val);
-//	}
 	@Test
-	void testFullHouse() {
+	void testStraightFlush() {
 		boolean asume = true;
 		boolean val;
-		videopoker.hand.add(new Card(10, Suit.HEARTS));
-		videopoker.hand.add(new Card(10, Suit.HEARTS));
-		videopoker.hand.add(new Card(10, Suit.HEARTS));
 		videopoker.hand.add(new Card(2, Suit.HEARTS));
-		videopoker.hand.add(new Card(2, Suit.HEARTS));
-		val = videopoker.isFullHouse(videopoker.hand);
+		videopoker.hand.add(new Card(3, Suit.HEARTS));
+		videopoker.hand.add(new Card(4, Suit.HEARTS));
+		videopoker.hand.add(new Card(5, Suit.HEARTS));
+		videopoker.hand.add(new Card(6, Suit.HEARTS));
+		val = videopoker.isStraightFlush(videopoker.hand);
 		assertEquals(asume, val);
 	}
+
+
+//	@Test
+//	void testFullHouse() {
+//		boolean asume = true;
+//		boolean val;
+//		videopoker.hand.add(new Card(10, Suit.HEARTS));
+//		videopoker.hand.add(new Card(10, Suit.HEARTS));
+//		videopoker.hand.add(new Card(10, Suit.HEARTS));
+//		videopoker.hand.add(new Card(2, Suit.HEARTS));
+//		videopoker.hand.add(new Card(2, Suit.HEARTS));
+//		val = videopoker.isFullHouse(videopoker.hand);
+//		assertEquals(asume, val);
+//	}
 }
