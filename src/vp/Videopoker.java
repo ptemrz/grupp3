72d3d6369b81dd9e7e141e561371626be383c115
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2019-12-06 09:30:12 +0100

    Cleaned up code in Royal Straight flush method. Also added comments to 3 methods

--- src/vp/Videopoker.java
+++ src/vp/Videopoker.java
@@ -139,63 +139,70 @@ public class Videopoker {
 						return false;
 					}	
 	}
-	
+	/**
+	 * Checks if the given hand is a Straight
+	 * 
+	 * @return true if the hand is a Straight
+	 * @param hand must be a sorted list of cards (assuming sorting by value)
+	 */	
 	public boolean isStraight(List<Card> hand) {
-		
-		int i = 0;
-			if (hand.get(i).getValue() == hand.get(i+1).getValue() + 1) {
-				if (hand.get(i+1).getValue() == hand.get(i+2).getValue() + 1) {
-					if (hand.get(i+2).getValue() == hand.get(i+3).getValue() + 1) {
-						if (hand.get(i+3).getValue() == hand.get(i+4).getValue() + 1){
-							return true;
-						}
-					}
-				}
-			} 
-			return false;
-	}
-			 
-	
-	
-	public boolean isStraightFlush(List<Card> hand) {
 
 		int i = 0;
-		if (hand.get(i).getValue() == (hand.get(i+1).getValue() + 1) && hand.get(i).getSuit() == hand.get(i+1).getSuit()) {
-			if (hand.get(i+1).getValue() == hand.get(i+2).getValue() + 1 && hand.get(i+1).getSuit() == hand.get(i+2).getSuit()) {
-				if (hand.get(i+2).getValue() == hand.get(i+3).getValue() + 1 && hand.get(i+2).getSuit() == hand.get(i+3).getSuit()) {
-					if (hand.get(i+3).getValue() == hand.get(i+4).getValue() + 1 && hand.get(i+3).getSuit() == hand.get(i+4).getSuit()){
+		if (hand.get(i).getValue() == hand.get(i + 1).getValue() + 1) {
+			if (hand.get(i + 1).getValue() == hand.get(i + 2).getValue() + 1) {
+				if (hand.get(i + 2).getValue() == hand.get(i + 3).getValue() + 1) {
+					if (hand.get(i + 3).getValue() == hand.get(i + 4).getValue() + 1) {
 						return true;
 					}
 				}
 			}
-		} 
+		}
 		return false;
 	}
+	/**
+	 * Checks if the given hand is a Straight flush
+	 * 
+	 * @return true if the hand is a Straight flush
+	 * @param hand must be a sorted list of cards (assuming sorting by value)
+	 */	
+	public boolean isStraightFlush(List<Card> hand) {
 
-	public boolean isRoyalStraightFlush(List<Card> hand) {
-		//Färg och stege 10-Ess
 		int i = 0;
-		if (hand.get(i).getValue() == 1 
-				/*|| hand.get(i).getValue() == 10 
-				|| hand.get(i).getValue() == 11 
-				|| hand.get(i).getValue() == 12 
-				|| hand.get(i).getValue() == 13*/) {
-			if (hand.get(i+1).getValue() == 10 
-					/*|| hand.get(i+1).getValue() == 11
-					|| hand.get(i+1).getValue() == 12
-					|| hand.get(i+1).getValue() == 13*/ && hand.get(i).getSuit() == hand.get(i+1).getSuit()) {
-				if (hand.get(i+2).getValue() == 11
-						/*|| hand.get(i+2).getValue() == 12
-						|| hand.get(i+2).getValue() == 13*/ && hand.get(i).getSuit() == hand.get(i+2).getSuit()) {
-					if (hand.get(i+3).getValue() == 12
-							/*|| hand.get(i+3).getValue() == 13*/ && hand.get(i).getSuit() == hand.get(i+3).getSuit()) {
-						if (hand.get(i+4).getValue() == 13 && hand.get(i).getSuit() == hand.get(i+4).getSuit()) {
-							return true;
-						}
-					}			
+		if (hand.get(i).getValue() == (hand.get(i + 1).getValue() + 1)
+				&& hand.get(i).getSuit() == hand.get(i + 1).getSuit()) {
+			if (hand.get(i + 1).getValue() == hand.get(i + 2).getValue() + 1
+					&& hand.get(i + 1).getSuit() == hand.get(i + 2).getSuit()) {
+				if (hand.get(i + 2).getValue() == hand.get(i + 3).getValue() + 1
+						&& hand.get(i + 2).getSuit() == hand.get(i + 3).getSuit()) {
+					if (hand.get(i + 3).getValue() == hand.get(i + 4).getValue() + 1
+							&& hand.get(i + 3).getSuit() == hand.get(i + 4).getSuit()) {
+						return true;
+					}
 				}
 			}
-		} 
+		}
+		return false;
+	}
+	/**
+	 * Checks if the given hand is a Royal Straight Flush
+	 * 
+	 * @return true if the hand is a Royal Straight Flush
+	 * @param hand must be a sorted list of cards (assuming sorting by value)
+	 */	
+	public boolean isRoyalStraightFlush(List<Card> hand) {
+		// Färg och stege 10-Ess
+		int i = 0;
+		if (hand.get(i).getValue() == 1) {
+			if (hand.get(i + 1).getValue() == 10 && hand.get(i).getSuit() == hand.get(i + 1).getSuit()) {
+				if (hand.get(i + 2).getValue() == 11 && hand.get(i).getSuit() == hand.get(i + 2).getSuit()) {
+					if (hand.get(i + 3).getValue() == 12 && hand.get(i).getSuit() == hand.get(i + 3).getSuit()) {
+						if (hand.get(i + 4).getValue() == 13 && hand.get(i).getSuit() == hand.get(i + 4).getSuit()) {
+							return true;
+						}
+					}
+				}
+			}
+		}
 		return false;
 	}
 }
