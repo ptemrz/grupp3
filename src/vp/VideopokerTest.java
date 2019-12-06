b3948303f5dff24632bc14f42c87b954efdd3e10
Anners <anners@mailzorz.com>
2019-12-06 11:38:45 +0100

    changed variable

--- src/vp/VideopokerTest.java
+++ src/vp/VideopokerTest.java
@@ -39,14 +39,14 @@ class VideopokerTest {
 	@Test
 	void testIsFlush() {
 
-		boolean ärDetFärg = true;
+		boolean asume = true;
 		videopoker.hand.add(new Card(1, Suit.HEARTS));
 		videopoker.hand.add(new Card(5, Suit.HEARTS));
 		videopoker.hand.add(new Card(3, Suit.HEARTS));
 		videopoker.hand.add(new Card(6, Suit.HEARTS));
 		videopoker.hand.add(new Card(2, Suit.HEARTS));
 
-		assertEquals(ärDetFärg, videopoker.isFlush(videopoker.hand));
+		assertEquals(asume, videopoker.isFlush(videopoker.hand));
 	}
 
 	@Test
